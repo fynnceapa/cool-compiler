ec49250bf28b1288faa8790c059666ebd99d0b4c
Teodor Dutu <teodor.dutu@gmail.com>
2024-12-27 18:45:01 +0200

    infra: Fix container name in `local.sh`
    Signed-off-by: Teodor Dutu <teodor.dutu@gmail.com>

--- local.sh
+++ local.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")" || exit 1
 RED='\033[0;31m'
 NC='\033[0m'
 
-DEFAULT_IMAGE_NAME="$(basename "$(pwd)")"
+DEFAULT_IMAGE_NAME="compilatoare/tema-3-generare-cod"
 DEFAULT_TAG='latest'
 DEFAULT_REGISTRY='gitlab.cs.pub.ro:5050'
 
